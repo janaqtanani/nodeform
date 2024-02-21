0a5a7c809e7d6e412e33f3f1682dbe257e1d7ab2
janaqtanani <158805339+janaqtanani@users.noreply.github.com>
2024-02-20 23:27:00 -0800

    Create form.js

---
+++ form.js
@@ -0,0 +1,30 @@
+
+const express= require('express')
+
+var app= express();
+
+//var json= require('json')
+
+app.get('/',function(req,res)
+{
+    res.send("start server")
+})
+
+app.get('/form', function(req,res)
+{
+    res.sendFile(__dirname+'/form.html')
+})
+
+var bodyParser= require('body-parser')
+
+var urlEncoded= bodyParser.urlencoded({extended:false})
+
+app.post('/submitform',urlEncoded,function(req,res){
+    res.send("data submitted"+req.unname+req.body.email+req.body.password)
+})
+
+var server= app.listen(7000, function()
+{
+    var host = server.address().address // local one  
+    var port = server.address().port 
+})
